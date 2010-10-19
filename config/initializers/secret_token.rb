1292aadd3fb2e8720511f8cf6276df666f2d2771
ethan.le <ethan.le@ufinity.com>
2010-10-19 22:25:51 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WayOfTheLight::Application.config.secret_token = 'bda9e7dba08e930050948a3eeceed51048faedadf2e877c172e5ba61a19a5343e036dff6342ab7be213dbc663cd362900c83b5f3baf5ecfd6225aea7fb2a3868'
